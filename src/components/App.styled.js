16ad49ffc765686304107500728cc895eb78d330
YurkoKuksa <145128983+YurkoKuksa@users.noreply.github.com>
2024-01-10 16:30:56 +0100

    i

--- src/components/App.styled.js
+++ src/components/App.styled.js
@@ -10,3 +10,15 @@ export const MainContainer = styled.div`
   color: #010101;
   background-color: #98f398;
 `;
+
+export const HeaderTwo = styled.h2`
+  color: #686880;
+  margin-bottom: 10px;
+  font-size: 70px;
+  font-weight: 700;
+  text-shadow: ${({ theme }) => theme.shadows.small};
+
+  &:hover {
+    color: #5b5885;
+  }
+`;
