d9cd3aebdabc190f224de2aec16db18217aaf4af
YurkoKuksa <145128983+YurkoKuksa@users.noreply.github.com>
2024-01-10 15:57:52 +0100

    initial_data

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from 'components/App';
+import './index.css';
+
+import 'modern-normalize/modern-normalize.css';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from 'styles/CreateGlobalStyle';
+import { theme } from 'styles/theme';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <ThemeProvider theme={theme}>
+    <App />
+    <GlobalStyle />
+  </ThemeProvider>
+);
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
